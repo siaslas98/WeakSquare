31c2c57c581a5d6aa00472f5a7723aa96c9fb993
siaslas <huang.treanthony@ufl.edu>
2026-01-28 22:38:16 -0500

    setup basic frontend and backend infrastructure

---
+++ app/backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
--- app/frontend/weak-square/src/App.jsx
+++ app/frontend/weak-square/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [status, setStatus] = useState(null);
+
+   useEffect(() => {
+    fetch("http://localhost:8000/health")
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
+        setStatus(data.status);
+      })
+      .catch(console.error);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="p-4">
+      <h1>WeakSquare</h1>
+      <p>Backend status: {status ?? "loading..."}</p>
+    </div>
+  );
 }
 
 export default App
